5974f259675a26810367760c1e7ce1aaa2ce9f1b
Sebastian Paiva <austral@gamerdeal.tv>
2013-09-14 12:37:39 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TecTV::Application.config.secret_key_base = 'a88df0f3abf181ec583e5fabe7eadc80686d3c8c7163ec06b1c125335b858cf3acfd3842393716dba1a5b8f2e02a80b09993e70f50834af9c75f5efe568b40e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TecTV::Application.config.session_store :cookie_store, key: '_TecTV_session'
